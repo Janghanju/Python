75d35f77f70b99d68dbb3d587d5a18a4612c3f62
Janghanju <hanju1215@naver.com>
2016-09-12 17:54:20 +0900

    Add files via upload

---
+++ 입출력.py
@@ -0,0 +1,5 @@
+A = input("입력해주세요.\n")   #입력값을 in_AAA로 칭한다
+                               #\n은 문법의 줄바꾸기
+
+print(A.upper()+" World!")           #in_AAA를 출력 + "World!")
+                                     #upper()는 앞의 값을 대문자화+"
---
+++ 조건문의 활용.py
@@ -0,0 +1,4 @@
+input = 22
+real = 22
+if real == input:
+    print("Hello!")
