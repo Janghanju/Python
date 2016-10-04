34ff144a703a73ff32e436daf4c071e450fedf8c
Janghanju <hanju1215@naver.com>
2016-10-04 17:13:59 +0900

    Add files via upload

---
+++ 리턴값.py
@@ -0,0 +1,18 @@
+def a3():
+    print("before")
+    return "aaa"        #함수의 결과값을 출력함, 리턴은 함수를 종료시킨다.
+    print("after")
+
+print(a3())
+
+#print(len("aa"))
+
+#print(1+1)
+
+#print(2)
+
+#print("a"*3)
+
+#print("aaa")
+
+#표현식, Expression 임
