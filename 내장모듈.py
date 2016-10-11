f8a2ba93fc16c5c2357731fa650499bb6d435fb3
Janghanju <hanju1215@naver.com>
2016-10-11 17:47:41 +0900

    Add files via upload

---
+++ 내장모듈.py
@@ -0,0 +1,4 @@
+import math
+print(math.ceil(2.1)) # 올림 정수를 리턴함
+print(math.floor(2.9))# 내림 정수를 리턴함
+print(math.sqrt(4))#루트식
---
+++ 모듈에 없을때.py
@@ -0,0 +1,8 @@
+def egoing_a():
+    return "A"
+#엄청 많은 코드
+def k8805_a():
+    return "B"
+#엄청 많은 코드
+print(egoing_a())
+
